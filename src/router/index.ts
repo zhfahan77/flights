f031d1246256b7c3e8a6c3fc9cbea9d0827ad7f1
Zamiul Hasan <zhfahan77@gmail.com>
2022-01-22 01:23:22 +0600

    feat: swagger docs added

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,26 @@
 import { Application } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 
 import { flightsRouter } from './flights';
 import { healthCheckRouter } from './health';
 
+const swaggerPassedOptions = {
+    swaggerOptions: {
+        filter: true,
+    },
+};
+
+const swaggerFilePath = path.join(__dirname, '../../swagger.yml');
+const swaggerDocument = YAML.load(swaggerFilePath);
+
 export const initializeRoutes = (app: Application) => {
+    app.use(
+        '/api/swagger-docs',
+        swaggerUi.serve,
+        swaggerUi.setup(swaggerDocument, swaggerPassedOptions)
+    );
     app.use('/api/flights', flightsRouter)
     app.use('/api/health', healthCheckRouter)
 };
