11c31150db44ffc27325fa8aa1e9b4891d11b375
Zamiul Hasan <zhfahan77@gmail.com>
2022-01-21 17:11:48 +0600

    feat: proper implementation of remove duplicate flights

--- src/core/flights/flights.ts
+++ src/core/flights/flights.ts
@@ -1,16 +1,40 @@
-import { FlightDetails, FlightDetailSource } from './types';
+import { FlightDetails, FlightDetailSource, Flights, Slice } from './types';
 import { Repository } from '../../repository/flights/types';
 
 export const _mergeFlights = (source1: FlightDetailSource, source2: FlightDetailSource): FlightDetailSource => {
     // concat is faster than spread: https://stackoverflow.com/a/57191593
+    // time complexity is O(N)
     const merged = source1.flights.concat(source2.flights);
     return {
         flights: merged
     }
 };
 
+const _generateFlightsMapHashKey = (f: Slice, s: Slice) => {
+    if (!s) {
+        return `${f.departure_date_time_utc}_${f.arrival_date_time_utc}_${f.flight_number}`;
+    };
+    return `${f.departure_date_time_utc}_${f.arrival_date_time_utc}_${f.flight_number}_${s.departure_date_time_utc}_${s.arrival_date_time_utc}_${s.flight_number}`
+};
+
 export const _removeDuplicateFlights = (data: FlightDetailSource): FlightDetailSource => {
-    return data
+    const flightsMap = new Map();
+    console.log('Total flights received: ', data.flights.length);
+
+    // creating a hash map, time complexity is O(N)
+    data.flights.forEach((flights: Flights) => {
+        const firstSlice = flights?.slices?.[0];
+        const secondSlice = flights?.slices?.[1];
+        // @ts-ignore
+        flightsMap.set(_generateFlightsMapHashKey(firstSlice, secondSlice), flights);
+    });
+
+    console.log("Total number of duplicates removed: ", data.flights.length - Array.from(flightsMap.values()).length);
+
+    // Map.values() returns an iterator, Array.from() iterates it over in O(N) time complexity
+    return {
+        flights: Array.from(flightsMap.values())
+    }
 };
 
 export const getFlights = async (repository: Repository): Promise<FlightDetailSource> => {
